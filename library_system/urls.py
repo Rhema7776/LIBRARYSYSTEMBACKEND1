d13e4d072cbb0d551f2143c4037287c8f7b539be
Rhema7776 <rhemachukwumah7@gmail.com>
2025-08-16 05:22:08 +0100

    Added a Http Response HomePage

--- library_system/urls.py
+++ library_system/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Library System API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
